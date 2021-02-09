d6f613d9c2386e147940fa210aa1e5a9594ec9cf
Stewart <stewarthkerr@gmail.com>
2021-02-08 23:25:55 -0500

    Added SRS variance to bootstrap results

--- code/India_LFS_bootstrap.R
+++ code/India_LFS_bootstrap.R
@@ -25,13 +25,16 @@ results = setNames(unique(employed$analysis_group), unique(employed$analysis_gro
   map(function(group) {
     d2analyze = employed[employed$analysis_group == group, ]
     b2 = boot(data = d2analyze, statistic = boot_mean, R = 1000)
-    out = c(levels(employed$analysis_group)[group], nrow(d2analyze), b2$t0, var(b2$t))
+    out = c(levels(employed$analysis_group)[group], nrow(d2analyze), b2$t0, var(b2$t), var(d2analyze$weekly_earnings) / nrow(d2analyze))
     return(out)
     })
 
 # Get results in a nice format
 results_df = as.data.frame(t(as.data.frame(results)), stringsAsFactors = FALSE) %>%
-  rename(group = V1, n = V2, estimate = V3, bootstrap_variance = V4) %>%
-  mutate(n = as.numeric(n), estimate = as.numeric(estimate), bootstrap_variance = as.numeric(bootstrap_variance))
+  rename(group = V1, n = V2, estimate = V3, bootstrap_variance = V4, SRS_variance = V5) %>%
+  mutate(n = as.numeric(n), estimate = as.numeric(estimate), bootstrap_variance = as.numeric(bootstrap_variance), SRS_variance = as.numeric(SRS_variance))
 rownames(results_df) = NULL  
-# TODO: Generalize this code
+
+# Save results
+results_df = arrange(results_df, desc(bootstrap_variance))
+write.csv(results_df, "../data/final/2011_India_LFS_bootstrap_results.csv", row.names = FALSE)
